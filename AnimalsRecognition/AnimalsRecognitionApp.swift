2fc4646d359ceec843543cc647699b67ab913960
Mohamad <ima.mawaldi@gmail.com>
2023-10-17 11:13:55 +0200

    Initial Commit

---
+++ AnimalsRecognition/AnimalsRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsRecognitionApp.swift
+//  AnimalsRecognition
+//
+//  Created by mohamad on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
